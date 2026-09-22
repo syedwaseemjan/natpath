186a43982e8938cf1a88c31f2c629264fd145286
Waseem Jan <syedwaseemjan@gmail.com>
2026-09-22 14:32:34 +0500

    Fake an AWS paginator so tests never call AWS.

---
+++ tests/fakes.py
@@ -0,0 +1,15 @@
+"""In-memory paginators. Tests never call AWS."""
+
+from typing import Any
+
+
+class Paginator:
+    def __init__(self, pages: list[dict[str, Any]]) -> None:
+        self.pages = pages
+        self.calls: list[dict[str, Any]] = []
+
+    def paginate(self, **kwargs: Any) -> list[dict[str, Any]]:
+        self.calls.append(kwargs)
+        return self.pages
+
+
