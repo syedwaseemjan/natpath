91d1c94cb129ce2e8013ae8919d755f1e43c42cc
Waseem Jan <syedwaseemjan@gmail.com>
2026-09-22 14:19:23 +0500

    Remove deprecated files and configurations, including linting tools, workflows, and application scripts, to streamline the project structure.

--- bin/_mypath.py
+++
@@ -1,6 +0,0 @@
-import os
-import sys
-
-PROJECT_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir))
-sys.path.append(PROJECT_DIR)
-sys.path.append(os.path.abspath(os.path.join(PROJECT_DIR, "app")))
--- bin/run_celery.py
+++
@@ -1,8 +0,0 @@
-#! /usr/bin/env python
-import _mypath  # noqa
-
-if __name__ == "__main__":
-    from app.tasks import app
-
-    worker = app.Worker(include=["app.tasks"])
-    worker.start()
